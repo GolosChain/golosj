46676256442e7b28374a5f0ba96266dc9e92ff4e
yuri <yurivladdurain@gmail.com>
2018-04-04 19:14:00 +1000

    hf 17 support

--- core/src/main/java/eu/bittrade/libs/steemj/Golos4J.java
+++ core/src/main/java/eu/bittrade/libs/steemj/Golos4J.java
@@ -86,10 +86,7 @@ public class Golos4J {
             steemJConfig.setResponseTimeout(180_000);
             steemJConfig.setSocketTimeout(180_000);
             try {
-                 steemJConfig.setWebSocketEndpointURI(new URI("wss://ws.golos.io"));
-              //    steemJConfig.setWebSocketEndpointURI(new URI("wss://ws17.golos.io"));
-                // steemJConfig.setWebSocketEndpointURI(new URI("wss://ws.testnet.golos.io"));
-              //  steemJConfig.setWebSocketEndpointURI(new URI("ws://78.46.193.218:8091"));
+                  steemJConfig.setWebSocketEndpointURI(new URI("wss://ws17.golos.io"));
             } catch (URISyntaxException e) {
                 e.printStackTrace();
             }
